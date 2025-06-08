2ea617c278b05545a8ab974591894c49f9c4926f
PedroDev <pedrohenriquelouresoliveira@gmail.com>
2025-06-08 19:29:30 -0300

    Estrutura inicial

---
+++ src/app/Index/index.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export default function Index() {
+    return (
+        <View>
+            <Text>Welcome to the Index Page</Text>
+        </View>
+    )
+}
